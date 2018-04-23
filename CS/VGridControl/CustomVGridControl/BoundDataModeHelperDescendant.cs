38311f6bfea0a13364d710d6a8284c7d468dc8e9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/VGridControl/CustomVGridControl/BoundDataModeHelperDescendant.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.XtraVerticalGrid.Data;
+
+namespace E4423
+{
+    public class CustomDataModeHelper : BoundDataModeHelper
+    {
+        public CustomDataModeHelper(VGridDataManager manager)
+            : base(manager)
+        {
+
+        }
+
+        public override bool NewItemRecordMode
+        {
+            get
+            {
+                if ((this.Manager.CurrentClient as VGridControlDescendant).AllowNew)
+                    return true;
+                return false;
+            }
+        }
+       
+        public override int Position
+        {
+            get
+            {
+                if(Manager.CurrentControllerRow == -2147483648) 
+                    return RecordCount - 1;
+				return Manager.CurrentControllerRow;
+            }
+        }
+    }
+}
---
+++ VB/VGridControl/SomeList.vb
@@ -0,0 +1,45 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+
+Namespace E4423
+	Public Class SomeList
+		Public Sub New()
+		End Sub
+		Public Sub New(ByVal _number As Integer, ByVal _name As String, ByVal _something As String)
+			Number = _number
+			Name = _name
+			Something = _something
+
+		End Sub
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+		Private privateSomething As String
+		Public Property Something() As String
+			Get
+				Return privateSomething
+			End Get
+			Set(ByVal value As String)
+				privateSomething = value
+			End Set
+		End Property
+	End Class
+End Namespace
